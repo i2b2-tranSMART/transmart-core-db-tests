0d80fa6cde1e97896b4ea581c48abd1941165358
Burt Beckwith <burt@burtbeckwith.com>
2018-03-26 15:46:02 -0400

    added artifactory repos

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,3 @@
-grails.plugin.location.'transmart-core-db' = '../.'
-
 grails.project.work.dir = 'target'
 
 grails.project.dependency.resolver = 'maven'
@@ -12,6 +10,8 @@ grails.project.dependency.resolution = {
 		mavenLocal() // Note: use 'grails maven-install' to install required plugins locally
 		grailsCentral()
 		mavenCentral()
+		mavenRepo 'http://ec2-35-170-59-132.compute-1.amazonaws.com:8080/artifactory/plugins-snapshots'
+		mavenRepo 'http://ec2-35-170-59-132.compute-1.amazonaws.com:8080/artifactory/plugins-releases'
 		mavenRepo 'https://repo.transmartfoundation.org/content/repositories/public/'
 	}
 
@@ -34,6 +34,10 @@ grails.project.dependency.resolution = {
 	}
 
 	plugins {
+
+		compile ':hibernate:3.6.10.19', { export = false }
+		compile ':transmart-core:18.1-SNAPSHOT'
+
 		build ':release:3.1.2', ':rest-client-builder:2.1.1', {
 			export = false
 		}
